eac0310f7511396833aa020a18e37d62d15bbe04
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2023-01-30 10:37:35 +0600

    added cors

--- index.js
+++ index.js
@@ -42,6 +42,16 @@ async function verifyToken(req, res, next) {
   next();
 }
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 async function run() {
   try {
     //delete await for vercel before client.connent()
