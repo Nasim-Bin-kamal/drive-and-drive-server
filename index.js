2d9d6fcbe6758e714d514ed50609e4de064009f4
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2023-02-04 00:31:58 +0600

    new env variable added

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ admin.initializeApp({
 const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzd4k.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
